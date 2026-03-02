0fee5aa8b889f5f00bd5b4569c3783ec291b69ac
Joseph Mateo Trespalacios Luque <jmateotrespalacios@unicesar.edu.co>
2026-03-02 17:58:44 -0500

    feat: Crear clase estudiante con atributos

---
+++ src/main/java/com/gilgarzonj/sistemaacademico/Estudiante.java
@@ -0,0 +1,14 @@
+package com.gilgarzonj.sistemaacademico;
+
+public class Estudiante {
+
+    private String codigo;
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private int semestre;
+
+  
+    
+
+}
