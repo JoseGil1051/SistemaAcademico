4291e84dd307eba20072b30ff2e2f787d78976d1
Jose Miguel Gil <jmiguelgil@unicesar.edu.co>
2026-03-02 16:34:20 -0500

    feat: inicializar proyecto SistemaAcademico

---
+++ src/main/java/com/gilgarzonj/sistemaacademico/SistemaAcademico.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.gilgarzonj.sistemaacademico;
+
+/**
+ *
+ * @author gilga
+ */
+public class SistemaAcademico {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
