abf1f085a583d95bd399386e11807875548c407b
Jose Hernandez <jdhernandezmercado@unicesar.edu.co>
2026-03-02 20:44:39 -0500

    feature: crear clase Nota con atributos

---
+++ src/main/java/com/gilgarzonj/sistemaacademico/Nota.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.gilgarzonj.sistemaacademico;
+
+/**
+ *
+ * @author HP
+ */
+public class Nota {
+    private Estudiante estudiante;
+    private Asignatura asignatura;
+    private double valor;
+    private String periodo;
+    
+}
